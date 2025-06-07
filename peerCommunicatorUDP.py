efd078f68259f6bc482da8b8fe79db62d462dbba
MauricioMoraesP <mmprogrammer@hotmail.com>
2025-06-07 19:16:30 -0300

    Ajustes

--- peerCommunicatorUDP.py
+++ peerCommunicatorUDP.py
@@ -60,7 +60,7 @@ class MsgHandler(threading.Thread):
         self.sock = sock
 
     def run(self):
-        print('Handler is ready. Waiting for the handshakes...')
+        print('[LOG] Handler thread started. Waiting for handshakes...')
         global handShakeCount
         global LIST_MESSAGES
         
@@ -70,9 +70,10 @@ class MsgHandler(threading.Thread):
             msg = pickle.loads(msgPack)
             if msg[0] == 'READY':
                 handShakeCount += 1
-                print('--- Handshake received: ', msg[1])
+                print('[LOG] Received handshake from process', msg[1], 
+                      '| Total handshakes:', handShakeCount, '/', N)
 
-        print('Secondary Thread: Received all handshakes. Entering the loop to receive messages.')
+        print('[LOG] All handshakes received. Starting message processing...')
 
         stopCount = 0 
         while True:                
@@ -80,36 +81,45 @@ class MsgHandler(threading.Thread):
             msg = pickle.loads(msgPack)
             if msg[0] == -1:   # count the 'stop' messages
                 stopCount += 1
+                print('[LOG] Received STOP signal from process', msg[1], 
+                      '| Total STOPs:', stopCount, '/', N)
                 if stopCount == N:
                     break
             else:
-                print('Message ' + str(msg[1]) + ' from process ' + str(msg[0]))
-                # Adiciona mensagem à lista global
+                print('[LOG] Received message', msg[1], 'from process', msg[0])
                 LIST_MESSAGES.append(msg)
+                print('[LOG] Current message list size:', len(LIST_MESSAGES))
         
-        # Ordena todas as mensagens antes de enviar
-        # Primeiro pelo ID do processo, depois pelo número da mensagem
+        # Ordenação das mensagens
+        print('[LOG] Starting message sorting...')
         LIST_MESSAGES.sort(key=lambda x: (x[0], x[1]))
+        print('[LOG] Sorting completed. First few messages:')
+        for msg in LIST_MESSAGES[:5]:  # Mostra as primeiras 5 mensagens como amostra
+            print('   ', msg)
         
-        # Write log file with ordered messages
+        # Write log file
         logFile = open('logfile'+str(myself)+'.log', 'w')
         logFile.writelines(str(LIST_MESSAGES))
         logFile.close()
+        print('[LOG] Messages saved to log file')
         
-        # Send the ordered list of messages to the server
-        print('Sending the ordered list of messages to the server for comparison...')
+        # Send to server
+        print('[LOG] Preparing to send ordered messages to server...')
         clientSock = socket(AF_INET, SOCK_STREAM)
         clientSock.connect((SERVER_ADDR, SERVER_PORT))
         msgPack = pickle.dumps(LIST_MESSAGES)
         clientSock.send(msgPack)
         clientSock.close()
+        print('[LOG] Ordered messages sent to server')
         
-        # Reset counters for next run
+        # Reset
         handShakeCount = 0
         LIST_MESSAGES = []
+        print('[LOG] Handler thread finished')
         exit(0)
 
 def waitToStart():
+    print('[LOG] Waiting for start signal from server...')
     (conn, addr) = serverSock.accept()
     msgPack = conn.recv(1024)
     msg = pickle.loads(msgPack)
@@ -117,50 +127,60 @@ def waitToStart():
     nMsgs = msg[1]
     conn.send(pickle.dumps('Peer process '+str(myself)+' started.'))
     conn.close()
+    print('[LOG] Start signal received. My ID:', myself, '| Messages to send:', nMsgs)
     return (myself, nMsgs)
 
 # Main execution
+print('[LOG] Starting peer process...')
 registerWithGroupManager()
 while True:
-    print('Waiting for signal to start...')
+    print('\n[LOG] ===== NEW ITERATION =====')
     (myself, nMsgs) = waitToStart()
-    print('I am up, and my ID is: ', str(myself))
+    print('[LOG] I am process', myself)
 
     if nMsgs == 0:
-        print('Terminating.')
+        print('[LOG] Termination signal received. Exiting...')
         exit(0)
 
-    # Wait for other processes to be ready
     time.sleep(5)
-
-    # Create receiving message handler
+    print('[LOG] Creating message handler...')
     msgHandler = MsgHandler(recvSocket)
     msgHandler.start()
-    print('Handler started')
 
     PEERS = getListOfPeers()
+    print('[LOG] Peer list obtained:', PEERS)
     
     # Send handshakes
+    print('[LOG] Sending handshakes to all peers...')
     for addrToSend in PEERS:
-        print('Sending handshake to ', addrToSend)
+        print('   Sending to:', addrToSend)
         msg = ('READY', myself)
         msgPack = pickle.dumps(msg)
         sendSocket.sendto(msgPack, (addrToSend, PEER_UDP_PORT))
 
-    print('Main Thread: Sent all handshakes. handShakeCount=', str(handShakeCount))
-
+    print('[LOG] All handshakes sent. Waiting for responses...')
     while handShakeCount < N:
-        pass  # wait for all handshakes
+        time.sleep(0.1)  # Evita CPU spinning
+    print('[LOG] All handshakes confirmed. Proceeding to send messages...')
 
-     for msgNumber in range(0, nMsgs):
-        time.sleep(random.randrange(10,100)/1000)
+    # Send data messages
+    print('[LOG] Starting to send', nMsgs, 'messages to each peer...')
+    for msgNumber in range(0, nMsgs):
+        delay = random.randrange(10,100)/1000
+        time.sleep(delay)
         msg = (myself, msgNumber)
         msgPack = pickle.dumps(msg)
         for addrToSend in PEERS:
             sendSocket.sendto(msgPack, (addrToSend, PEER_UDP_PORT))
-            print('Sent message ' + str(msgNumber))
-
-     for addrToSend in PEERS:
+        print('   Sent message', msgNumber, 'to all peers | Delay:', delay)
+    
+    # Send stop signals
+    print('[LOG] Sending STOP signals to all peers...')
+    for addrToSend in PEERS:
         msg = (-1, -1)
         msgPack = pickle.dumps(msg)
         sendSocket.sendto(msgPack, (addrToSend, PEER_UDP_PORT))
+    print('[LOG] All STOP signals sent. Waiting for handler to finish...')
+    
+    msgHandler.join()  # Espera a thread handler terminar
+    print('[LOG] Handler finished. Ready for next iteration.')
